e73d6841a4642e3d67a05c36f3060d2266884ef2
ranashreyas <ranashreyas@gmail.com>
2024-03-10 21:06:14 -0700

    taskbar ui changes for enable and disable buttons

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { INotebookTracker } from '@jupyterlab/notebook';
+
+import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
+import { ToolbarButton } from '@jupyterlab/apputils';
 import { MarkdownCell, ICellModel } from '@jupyterlab/cells';
 import { IMainMenu } from '@jupyterlab/mainmenu';
-import { Menu } from '@lumino/widgets';
+import { IDisposable } from '@lumino/disposable';
+// import { Menu } from '@lumino/widgets';
+
 
 function checkHtmlNoAlt(htmlString: string): boolean {
   const parser = new DOMParser();
@@ -38,15 +42,73 @@ function attachContentChangedListener(cell: MarkdownCell, isEnabled: () => boole
   cell.model.contentChanged.connect(() => {
     if (isEnabled()){
       const hasImageWithoutAlt = checkMarkdownCellForImageWithoutAlt(cell);
-      if (hasImageWithoutAlt) {
-        cell.node.style.backgroundColor = '#ff8080';
-      } else {
-        cell.node.style.backgroundColor = '';
-      }
+      applyVisualIndicator(cell, hasImageWithoutAlt);
     } else {
-      cell.node.style.backgroundColor = '';
+      applyVisualIndicator(cell, false);
     }
   });
+}
+
+function applyVisualIndicator(cell: MarkdownCell, applyIndic: boolean) {
+  const indicatorId = `accessibility-indicator-${cell.model.id}`;
+
+  if (applyIndic) {
+    let indicator = document.createElement('div');
+    indicator.id = indicatorId;
+    indicator.style.position = 'absolute';
+    indicator.style.top = '12px';
+    indicator.style.left = '44px';
+    indicator.style.width = '15px';
+    indicator.style.height = '15px';
+    indicator.style.borderRadius = '50%';
+    indicator.style.backgroundColor = '#ff8080';
+    cell.node.appendChild(indicator);
+  } else {
+    let indicator = document.getElementById(indicatorId);
+    indicator?.remove();
+  }
+}
+
+function addToolbarButton(notebookPanel: NotebookPanel, isEnabled: () => boolean, toggleEnabled: () => void): IDisposable {
+  const button = new ToolbarButton({
+    // className: 'my-altTextCheck-button',
+    label: 'Alt Text Check',
+    onClick: () => {
+      toggleEnabled();
+      updateButtonAppearance(button, isEnabled());
+      notebookPanel.content.widgets.forEach(cell => {
+        if (cell.model.type === 'markdown') {
+          const markdownCell = cell as MarkdownCell;
+          if (isEnabled()) {
+            const hasImageWithoutAlt = checkMarkdownCellForImageWithoutAlt(markdownCell);
+            applyVisualIndicator(markdownCell, hasImageWithoutAlt);
+          } else {
+            applyVisualIndicator(markdownCell, false);
+          }
+        }
+      });
+    },
+
+    tooltip: 'Toggle Alt-text Check'
+  });
+
+  button.id = "alt-text-check-toggle";
+  notebookPanel.toolbar.insertItem(10, 'altTextCheck', button);
+  
+  let elem = document.getElementById('alt-text-check-toggle');
+  elem!.style.backgroundColor = '#5c94ed';
+
+  return button;
+}
+
+function updateButtonAppearance(button: ToolbarButton, isOn: boolean) {
+  if (isOn) {
+    let elem = document.getElementById('alt-text-check-toggle');
+    elem!.style.backgroundColor = '#5c94ed';
+  } else {
+    let elem = document.getElementById('alt-text-check-toggle');
+    elem!.style.backgroundColor = '#0000';
+  }
 }
 
 const plugin: JupyterFrontEndPlugin<void> = {
@@ -56,42 +118,18 @@ const plugin: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd, notebookTracker: INotebookTracker, mainMenu: IMainMenu) => {
     console.log('JupyterLab extension jupyterlab_accessibility is activated!');
     
-    let isEnabled = true;
+    let isEnabled = true; // Flag to track the extension's enabled state
 
-    const accessibilityMenu = new Menu({ commands: app.commands });
-    accessibilityMenu.title.label = 'Accessibility';
-    mainMenu.addMenu(accessibilityMenu);
+    // Function to toggle the isEnabled state
+    const toggleEnabled = () => {
+      isEnabled = !isEnabled;
+      console.log(`Accessibility checks ${isEnabled ? 'enabled' : 'disabled'}.`);
+    };
 
-    const toggleCommand = 'jupyterlab_accessibility:toggle';
-    app.commands.addCommand(toggleCommand, {
-      label: 'Toggle Accessibility Checks',
-      isToggled: () => isEnabled,
-      execute: () => {
-        isEnabled = !isEnabled;
-        console.log(`Accessibility checks ${isEnabled ? 'enabled' : 'disabled'}.`);
-
-        notebookTracker.forEach(notebookPanel => {
-          notebookPanel.content.widgets.forEach(cell => {
-            if (cell.model.type === 'markdown') {
-              const markdownCell = cell as MarkdownCell;
-              if (!isEnabled) {
-                markdownCell.node.style.backgroundColor = '';
-              } else {
-
-                const hasImageWithoutAlt = checkMarkdownCellForImageWithoutAlt(markdownCell);
-                if (hasImageWithoutAlt) {
-                  markdownCell.node.style.backgroundColor = '#ff8080';
-                } else {
-                  markdownCell.node.style.backgroundColor = '';
-                }
-              }
-            }
-          });
-        });
-      }
+    // When a new notebook is created or opened, add the toolbar button
+    notebookTracker.widgetAdded.connect((sender, notebookPanel: NotebookPanel) => {
+      addToolbarButton(notebookPanel, () => isEnabled, toggleEnabled);
     });
-
-    accessibilityMenu.addItem({ command: toggleCommand });
 
     notebookTracker.currentChanged.connect((sender, notebookPanel) => {
       if (!notebookPanel) return;
@@ -99,24 +137,23 @@ const plugin: JupyterFrontEndPlugin<void> = {
       notebookPanel.context.ready.then(() => {
         const { content } = notebookPanel;
 
+        //for each existing cell, attach a content changed listener
         content.widgets.forEach(cell => {
           if (cell.model.type === 'markdown') {
             const markdownCell = cell as MarkdownCell;
             attachContentChangedListener(markdownCell, () => isEnabled);
             
+            //for each existing cell, check the accessibility once to initially flag it or not
             if (isEnabled) {
               const hasImageWithoutAlt = checkMarkdownCellForImageWithoutAlt(markdownCell);
-              if (hasImageWithoutAlt) {
-                markdownCell.node.style.backgroundColor = '#ff8080';
-              } else {
-                markdownCell.node.style.backgroundColor = '';
-              }
+              applyVisualIndicator(markdownCell, hasImageWithoutAlt);
             } else {
-              markdownCell.node.style.backgroundColor = '';
+              applyVisualIndicator(markdownCell, false);
             }
           }
         });
 
+        //every time a cell is added, attach a content listener to it
         if (content.model) {
           content.model.cells.changed.connect((sender, args) => {
             if (args.type === 'add') {
